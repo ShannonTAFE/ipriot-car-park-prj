66f1a004e6a40c216de8498b6f9e0f1e0fb49717
Shannon Robinson <20075186@tafe.wa.edu.au>
2025-07-10 19:43:15 +0800

    Added Display and Sensor class attributes and constructors 2.5.2, 2.5.3

--- src/display.py
+++ src/display.py
@@ -1,2 +1,10 @@
 class Display:
-    pass
+
+    def __init__(self, id, message="",is_on=False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+
+
+    def __str__(self):
+        return f"{self.id}: Display is {"on" if self.is_on else "off"}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,16 @@
 class Sensor:
+
+    def __init__(self, id, car_park, is_active=False):
+        self.id = id
+        self.car_park = car_park
+        self.is_active = is_active
+
+    def __str__(self):
+        return f"Sensor ID {self.id}, Status: {self.is_active}"
+
+class EntrySensor(Sensor):
     pass
+
+class ExitSensor(Sensor):
+    pass
+
