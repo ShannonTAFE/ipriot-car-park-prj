434f732e81ec22bf4e9e63d24c881bb87ef07cc0
Shannon Robinson <20075186@tafe.wa.edu.au>
2025-07-13 15:15:29 +0800

    Added a register, add, remove, update method in car_park

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,6 @@
+from sensor import Sensor
+from display import Display
+
 class CarPark:
 
     def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
@@ -10,3 +13,24 @@ class CarPark:
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays."
 
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object mst be a Sensor or Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
+
+
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+        self.update_displays()
+
+    def update_displays(self):
+        for display in self.displays:
+            display.update()
+            print(f"Updating: {display}")
