8a2a84d71c92da8235a78987c7256af02f6acd28
Shannon Robinson <20075186@tafe.wa.edu.au>
2025-07-21 21:43:26 +0800

    added unit tests for the car park class

--- src/sensor.py
+++ src/sensor.py
@@ -33,7 +33,7 @@ class ExitSensor(Sensor):
 
     def update_car_park(self, plate):
         self.car_park.remove_car(plate)
-        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}"
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
 
     def _scan_plate(self):
         return random.choice(self.car_park.plates)
