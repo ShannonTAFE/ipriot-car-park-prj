45b5a343b1f7b7f1b8ae844ff2ec958d447b753c
Shannon Robinson <20075186@tafe.wa.edu.au>
2025-07-28 09:50:58 +0800

    added log test and log to carpark, v.1

--- src/car_park.py
+++ src/car_park.py
@@ -1,14 +1,20 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime # we'll use this to timestamp entries
 
 class CarPark:
 
-    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays."
@@ -25,15 +31,21 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log(f"{datetime.now()}: {plate} entered\n")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log(f"{datetime.now()}: {plate} exited\n")
 
     def update_displays(self):
         for display in self.displays:
             display.update({"available_bays": self.available_bays, "temperature": 25})
             print(f"Updating: {display}")
+
+    def _log(self, message):
+        with self.log_file.open("a") as f:
+            f.write(message)
 
     @property
     def available_bays(self):
